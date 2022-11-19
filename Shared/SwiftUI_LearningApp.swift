0245feb8733698a29760f63c38d98783973818d1
Rushabh <rushabh@kshantechsoft.com>
2022-11-19 09:53:19 +0530

    Initial Commit

---
+++ Shared/SwiftUI_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LearningApp.swift
+//  Shared
+//
+//  Created by Rushabh on 11/19/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
